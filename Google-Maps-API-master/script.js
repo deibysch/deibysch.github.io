d42a918cada0ca6c21a72b69e28a85bc63642a54
deibysch <63740768+deibysch@users.noreply.github.com>
2022-06-03 17:11:53 -0400

    Update script.js

--- Google-Maps-API-master/script.js
+++ Google-Maps-API-master/script.js
@@ -1,11 +1,13 @@
 function iniciarMap(){
-    var coord = {lat:-34.5956145 ,lng: -58.4431949};
-    var map = new google.maps.Map(document.getElementById('map'),{
-      zoom: 10,
-      center: coord
-    });
-    var marker = new google.maps.Marker({
-      position: coord,
-      map: map
-    });
+    var coord = {lat:-17.783847435697183, lng: -63.18045008255558};
+    //console.log(map.maps)
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition((loc)=>{
+        coord.lat=loc.coords.latitude
+        coord.lng=loc.coords.longitude
+        console.log("pasaaaaaaaaaaa")
+        var map = new google.maps.Map(document.getElementById('map'),{zoom: 12, center: coord});
+        var marker = new google.maps.Marker(position=coord, map=map);
+      })
+    }
 }
